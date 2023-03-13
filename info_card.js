fa7fa897586f697ede13e7659a8628bab6c71733
Viewls2 <mahasamut.kh@gmail.com>
2023-03-13 16:25:28 +0700

    feat(1): mathematic functions and myInfo function

---
+++ info_card.js
@@ -0,0 +1,4 @@
+function myInfo(firstName,location,hobby) {
+    console.log("Hi, my name is " + firstName + ". I live in " + location + " and enjoy "+hobby+'.')
+}
+myInfo('Mahasamut','Bangkok','listen to music')
---
+++ math_fn.js
@@ -0,0 +1,27 @@
+// add x-y
+function add(x,y) {
+    return x + y;
+}
+a =add(2,4);
+console.log(a);
+
+// subtract x-y
+function subtract(x,y) {
+    return x - y;
+}
+b =subtract(5,2);
+console.log(b);
+
+// multiply x-y
+function multiply(x,y) {
+    return x * y;
+}
+c =multiply(2,4);
+console.log(c);
+
+// divide x/y
+function divide(x,y) {
+    return x / y;
+}
+d =divide(10,2);
+console.log(d);
